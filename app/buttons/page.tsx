2724fbed165f9a35df504688ab0eeb760e139253
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-18 11:08:47 +0400

    Creating variants for button

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 const page = () => {
   return (
     <div className='p-4 space-y-4 flex flex-col max-w-[200px]'>
-        <Button>Primary</Button>
+        <Button>Default</Button>
+
+        <Button variant="primary">Primary</Button>
+
+
         <Button>Primary Outline</Button>
 
     </div>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
-
+         primary:"bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0"
       },
       size: {
         default: "h-11 px-4 py-2",
