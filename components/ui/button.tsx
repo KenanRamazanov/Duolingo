3c4672a30e45a94083880713085f9b026f096291
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-18 21:40:34 +0400

    Creating super,superrOutline variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,10 @@ const buttonVariants = cva(
          secondaryOutline:"bg-white text-green-500 hover:bg-slate-100",
 
          danger:"bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
-         dangerOutline:"bg-white text-rose-500 hover:bg-slate-100"
+         dangerOutline:"bg-white text-rose-500 hover:bg-slate-100",
+
+         super:"bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
+         superOutline:"bg-white text-indigo-500 hover:bg-slate-100"
       },
       size: {
         default: "h-11 px-4 py-2",
