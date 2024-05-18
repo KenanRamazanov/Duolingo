a58fb55e577c2946efb844d598dfb059667076b1
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-18 10:52:57 +0400

    Changing button design

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='p-4 space-y-4'>
+    <div className='p-4 space-y-4 flex flex-col max-w-[200px]'>
         <Button>Primary</Button>
         <Button>Primary Outline</Button>
 
