24850e739ad216d0c0ac685655f0413dc56c0362
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-19 20:56:07 +0400

    Editing page

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -1,32 +1,30 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='p-4 space-y-4 flex flex-col max-w-[200px]'>
-        <Button>Default</Button>
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>Default</Button>
 
-        <Button variant="primary">Primary</Button>
+      <Button variant="primary">Primary</Button>
 
+      <Button variant="primaryOutline">Primary Outline</Button>
 
-        <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
 
-        <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
 
-        <Button variant="secondaryOutline">Secondary Outline</Button>
-   
-        <Button variant="danger">Danger</Button>
+      <Button variant="danger">Danger</Button>
 
-        <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
 
+      <Button variant="super">Super</Button>
 
-        <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
 
-        <Button variant="superOutline">Super Outline</Button>
-
-        <Button variant="ghost">Ghost</Button>
+      <Button variant="ghost">Ghost</Button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
