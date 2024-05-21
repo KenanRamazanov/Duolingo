191de8935cb2dea72aa90e0b03b9498bde84195f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-21 23:47:11 +0400

    Editing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-return (
- <Button size="lg" >Click me</Button>
-)
+  return <Button size="lg">Click me</Button>;
 }
