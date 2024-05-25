bf0c5a595eed17a55f99f38069066f272735774e
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-25 23:48:03 +0400

    Editing header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <header className='h-20 w-full border-b-2 border-slate-200 px-4'>
-        <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500' >
-          
-        </div>
-
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500"></div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
