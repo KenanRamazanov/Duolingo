54bf274d7bbd4946d00186df1dce25a0a6db2d88
kenan551 <kenan.ramazanov1104@gmail.com>
2024-05-31 20:40:43 +0400

    Creating logo

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500"></div>
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500">
+        <div className="pt-8 pl-4 flex items-center gap-x-3 ">
+         <Image src="/mascot.svg" width={40} height={40} alt="Mascot"/>
+        </div>
+      </div>
     </header>
   );
 };
