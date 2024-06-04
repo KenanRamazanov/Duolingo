e6476316f0e72c72ed5adcedf228f341e720b91c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-04 22:42:40 +0400

    Editing Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div
-     className='flex bg-blue-500 h-full lg:w-[256px] fixed '>
-        Sidebar
-    </div>
-  )
-}
+    <div className="flex bg-blue-500 h-full lg:w-[256px] fixed ">Sidebar</div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
