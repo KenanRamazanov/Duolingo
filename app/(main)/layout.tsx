6e6837c309ae406a2cb217be73146483764661cb
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-04 22:43:19 +0400

    Editing layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,20 +1,18 @@
 import Sidebar from "@/components/Sidebar";
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
-const MainLayout = ({children} : Props) => {
-    return (
-        <>
-        <Sidebar/>
-        <main className="pl-[256px]  h-full">
-            <div className="bg-red-500 h-full">
-            {children}
-            </div>
-        </main>
-        </>
-    );
+const MainLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Sidebar />
+      <main className="pl-[256px]  h-full">
+        <div className="bg-red-500 h-full">{children}</div>
+      </main>
+    </>
+  );
 };
 
 export default MainLayout;
