65fe4cb11ea16bd8f15a36d5e402de0a008a9ab8
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-08 23:36:01 +0400

    Editing footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
+
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
