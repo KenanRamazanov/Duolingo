0533f29ca957704d284cde884f5466ae1513cab4
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-08 23:35:22 +0400

    Editing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -11,6 +11,7 @@ import { Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
