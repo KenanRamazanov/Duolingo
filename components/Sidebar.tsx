c6371b0525aafe6a8f97051ee0365e75c3d26538
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-09 23:23:51 +0400

    Adding cn to Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import { cn } from "@/lib/utils";
 
-export const Sidebar = () => {
+type Props = {
+  className?: string;
+}
+export const Sidebar = ({className}: Props) => {
   return (
-    <div className="flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col">
+    <div className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col")}>
         Sidebar
     </div>
   );
