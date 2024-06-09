5e0670f3715afd5f6be963b3191321f23f071349
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-09 23:37:41 +0400

    Editing MobileHeader

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const MobileHeader = () => {
   return (
-    <div>mobile-header</div>
+    <nav className='lg:hidden px-6 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50'>
+      Mobile Header
+    </nav>
   )
 }
