69a0dd42a0cfa3f151fbe3ee3f8e12320f17de5c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-11 23:52:55 +0400

    Editing Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,22 +4,23 @@ import Link from "next/link";
 
 type Props = {
   className?: string;
-}
-export const Sidebar = ({className}: Props) => {
+};
+export const Sidebar = ({ className }: Props) => {
   return (
-    <div className={cn(
-    "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4  border-r-2 flex-col",className,
-    )}>
+    <div
+      className={cn(
+        "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4  border-r-2 flex-col",
+        className
+      )}
+    >
       <Link href="/">
-         <div className="pt-8 pl-4 flex items-center gap-x-3 ">
+        <div className="pt-8 pl-4 flex items-center gap-x-3 ">
           <Image src="/mascot.svg" width={40} height={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
             Duolingo
           </h1>
         </div>
-        </Link>
+      </Link>
     </div>
   );
 };
-
-
