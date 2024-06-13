25e2ae468159d0f5879124ac227d24626a374e2c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-13 22:11:11 +0400

    Editing SidebarItem

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -1,29 +1,18 @@
 "use client";
-import React from 'react'
-import { Button } from './ui/button';
-import { usePathname } from 'next/navigation';
-
+import React from "react";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 type Props = {
-    label: string;
-    iconSrc: string;
-    href: string;
+  label: string;
+  iconSrc: string;
+  href: string;
 };
 
-
-export const SidebarItem = ({
-    label,
-    iconSrc,
-    href
-}:Props) => {
-
-    const pathname = usePathname();
-    const active = pathname === href;
+export const SidebarItem = ({ label, iconSrc, href }: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
   return (
-    <Button
-    variant={active ?  "sidebarOutline" : "sidebar" }
-    >
-        {label}
-    </Button>
-  )
+    <Button variant={active ? "sidebarOutline" : "sidebar"}>{label}</Button>
+  );
 };
