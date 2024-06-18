ce9ee0a5e713c95f2e376b23ad7ffde8e2b57ac5
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-19 00:11:18 +0400

    Adding UserButton to Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { SidebarItem } from "./sidebar-item";
+import { ClerkLoading,ClerkLoaded,UserButton} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -43,7 +45,14 @@ export const Sidebar = ({ className }: Props) => {
          href="/shop"
          iconSrc="/shop.svg"
          />
-         
+      </div>
+      <div className="p-4">
+      <ClerkLoading>
+        <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <UserButton afterSignOutUrl="/"/>
+      </ClerkLoaded>
       </div>
     </div>
   );
