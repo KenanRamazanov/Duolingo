21925ae83cbb13540f851d4f24f783df912c73af
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-18 23:59:10 +0400

    Editing Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,18 +11,18 @@ export const Sidebar = ({ className }: Props) => {
     <div
       className={cn(
         "flex  h-full lg:w-[256px] lg:fixed left-0 top-0 px-4  border-r-2 flex-col",
-        className
+        className,
       )}
     >
       <Link href="/learn">
-        <div className="pt-8 pl-4 flex items-center gap-x-3 ">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3 ">
           <Image src="/mascot.svg" width={40} height={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
             Duolingo
           </h1>
         </div>
       </Link>
-      <div className="flex flex-col gap-y-2 flex-1">
+      <div className="flex flex-col gap-y-2 flex-1  ">
         <SidebarItem label="Learn" href="/learn" />
       </div>
     </div>
