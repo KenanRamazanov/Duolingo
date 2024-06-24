efba0edab24f1d009eb6b287c043b56268e49123
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-24 23:14:32 +0400

    Editing FeedWrapper

--- components/feed-wrapper.tsx
+++ components/feed-wrapper.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 type Props = {
-    children: React.ReactNode;
-}
-export const FeedWrapper = ({children} : Props) => {
-  return (
-    <div className='flex-1 relative top-0 pb-10'>
-        {children}
-    </div>
-  );
+  children: React.ReactNode;
 };
-
+export const FeedWrapper = ({ children }: Props) => {
+  return <div className="flex-1 relative top-0 pb-10">{children}</div>;
+};
