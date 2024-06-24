abf7d44acd3d9c0855ddbdb926c6d21ffea61da1
kenan551 <kenan.ramazanov1104@gmail.com>
2024-06-24 23:14:08 +0400

    Editing Header

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 type Props = {
-    title: string;
+  title: string;
 };
 
-export const Header = ({title} : Props) => {
+export const Header = ({ title }: Props) => {
   return (
-    <div className='sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center  justify-between] border-b-2 mb-5 text-neutral-400 lg:z-50'>
-    {title}
+    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center  justify-between] border-b-2 mb-5 text-neutral-400 lg:z-50">
+      {title}
     </div>
-  )
-}
-
+  );
+};
