4d6bc44441989917f9019f12fc29231fdacf5716
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-08 21:32:09 +0400

    Editing user-progress

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,8 +1,26 @@
+import Link from 'next/link';
 import React from 'react'
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
-export const UserProgress = () => {
+type Props = {
+    activeCourse: { imageSrc: string;  title: string  };
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+}
+export const UserProgress = ({activeCourse} : Props) => {
   return (
-    <div>UserProgress</div>
+    <div  className='flex items-center justify-between gap-x-2 w-full'>
+    <Link href="/courses">
+      <Button>
+       <Image
+       src={activeCourse.imageSrc}
+       alt={activeCourse.title}
+       />
+      </Button>
+    </Link>
+    </div>
   )
 }
 
