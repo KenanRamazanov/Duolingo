569a1195de9502fc6c37071ad6c264071a08c07b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-08 20:54:39 +0400

    Editing Header

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -8,12 +8,16 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
   return (
-    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center  justify-between] border-b-2 mb-5 text-neutral-400 lg:z-50">
+    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center  justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
       <Link href="/courses">
-      <Button>
+      <Button variant="ghost" size='sm'>
          <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400"/>
       </Button>
       </Link>
+      <h1 className="font-bold text-lg">
+        {title}
+      </h1>
+      <div/>
     </div>
   );
 };
