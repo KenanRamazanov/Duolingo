8ebc11de03a639b8f05ac96ae7321a4a07e7eb75
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-10 19:10:25 +0400

    Editing user-progress

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,40 +1,45 @@
-import Link from 'next/link';
-import React from 'react'
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import Link from "next/link";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 type Props = {
-    activeCourse: { imageSrc: string;  title: string  };
-    hearts: number;
-    points: number;
-    hasActiveSubscription: boolean;
-}
+  activeCourse: { imageSrc: string; title: string };
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
 export const UserProgress = ({
-    activeCourse,
-    points,
-    hearts,
-    hasActiveSubscription
-} : Props) => {
+  activeCourse,
+  points,
+  hearts,
+  hasActiveSubscription,
+}: Props) => {
   return (
-    <div  className='flex items-center justify-between gap-x-2 w-full'>
-    <Link href="/courses">
-      <Button variant="ghost">
-       <Image
-       src={activeCourse.imageSrc}
-       alt={activeCourse.title}
-       className='rounded-md border'
-       width={32}
-       height={32}
-       />
-      </Button>
-    </Link>
-    <Link href="/shop">
-    <Button variant="ghost" className='text-orange-500'>
-      <Image src="/points.svg" height={28} width={28} alt="Points" className='mr-2'/>
-      {points}
-    </Button>
-    </Link>
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            className="rounded-md border"
+            width={32}
+            height={32}
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            height={28}
+            width={28}
+            alt="Points"
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
     </div>
-  )
-}
-
+  );
+};
