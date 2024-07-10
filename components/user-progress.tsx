6f8e1261708a6ae877456f13d99fd6dcf1fce5af
kenan551 <kenan.ramazanov1104@gmail.com>
2024-07-10 19:24:45 +0400

    Editing hearts

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
 
 type Props = {
   activeCourse: { imageSrc: string; title: string };
@@ -44,12 +45,12 @@ export const UserProgress = ({
         <Button variant="ghost" className="text-rose-500">
           <Image
             src="/heart.svg"
-            height={28}
-            width={28}
-            alt="Points"
+            height={22}
+            width={22}
+            alt="Hearts"
             className="mr-2"
           />
-          {points}
+          {hasActiveSubscription ? <InfinityIcon className="h-4 w-4 stroke-[3]"/> :hearts}
         </Button>
       </Link>
     </div>
